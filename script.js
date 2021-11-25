0093c1688052efb26c7e3bc7b34bba09b03956b2
Eraze86 <therese.rosvall@hotmail.se>
2021-11-25 18:48:24 +0100

    fixed connection when clicking the button

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 // koppla input 1 och 2 till js
 
-const a = document.getElementById("num1").value;
-const b = document.getElementById("num2").value;
+
 //coppla button och slection till js
 
-const op = document.getElementById("option")
-const button = document.getElementById("button")
+const op = document.getElementById("option");
+const button = document.getElementById("button");
+const p = document.querySelector("p");
 
 
 //Skapa knappt som ska klicka igång uträkningen
 button.addEventListener("click", function(){ 
 
-
+    const a = parseInt(document.getElementById("num1").value);
+    const b = parseInt(document.getElementById("num2").value);
 //de olika räkne sätten
-    switch (op){
+    switch (op.value){
+ 
         case "add":
-          console.log(a + b)
+            p.innerText = (a+b)
+        //   console.log(a + b)
         break;
         case "sub":
-            console.log(a - b)
+            
+            p.innerText = (a - b)
         break;
         case "multi":
-            console.log(a * b)
+            
+            p.innerText = (a * b)
         break;
         case "divi":
-            console.log(a / b)
+            
+            p.innerText = (a / b)
         break;
     }
 });
