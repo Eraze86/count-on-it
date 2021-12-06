81700ca5d5f5daa548db0a102522d15124643193
Eraze86 <therese.rosvall@hotmail.se>
2021-12-06 20:40:59 +0100

    resultat visas i rader och stannar kvar tills man uppdaterar

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-// koppla input 1 och 2 till js
-
-
-//coppla button och slection till js
+//koppla button och slection till js
 
 const op = document.getElementById("option");
 const button = document.getElementById("button");
 const p = document.querySelector("p");
-
+const res = document.getElementById("res");
 
 //Skapa knappt som ska klicka igång uträkningen
 button.addEventListener("click", function(){ 
 
     const a = parseInt(document.getElementById("num1").value);
     const b = parseInt(document.getElementById("num2").value);
+    let resultat = document.createElement("div");
+    res.append(resultat)
 //de olika räkne sätten
     switch (op.value){
- 
+        
         case "add":
-            resultat.innerText = (a+b)
-        //   console.log(a + b)
+    
+            resultat.innerText += (a+b)
+         
         break;
         case "sub":
             
-            resultat.innerText = (a - b)
+            resultat.innerText += (a - b)
         break;
         case "multi":
             
-            resultat.innerText = (a * b)
+            resultat.innerText += (a * b)
         break;
         case "divi":
             
-            resultat.innerText = (a / b)
+            resultat.innerText += (a / b)
         break;
+        
     }
 });
